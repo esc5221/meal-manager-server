d324bb3e2a633c2bf0e9b10bce0ea4611168a7db
최병욱 <esc5221@gmail.com>
2023-06-06 17:10:15 +0900

    [feat] add food app, route urls

--- meal_manager/api.py
+++ meal_manager/api.py
@@ -2,9 +2,8 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
+from food.views import router as food_router
 from meal.views import router as meal_router
 
+api.add_router("food/", food_router)
 api.add_router("meal/", meal_router)
-
-
-
