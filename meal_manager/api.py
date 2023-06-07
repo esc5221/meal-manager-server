7aff04574e0b736e1123beb29f5c45913e7cf777
최병욱 <esc5221@gmail.com>
2023-06-07 23:38:27 +0900

    [feat] add api_exception_handler

--- meal_manager/api.py
+++ meal_manager/api.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from ninja import NinjaAPI
 
 api = NinjaAPI()
@@ -7,3 +8,12 @@ from meal.views import router as meal_router
 
 api.add_router("food/", food_router)
 api.add_router("meal/", meal_router)
+
+from meal_manager.exceptions import api_exception_response
+
+if not settings.DEBUG:
+
+    @api.exception_handler(Exception)
+    def api_exception_handler(request, exc):
+        response, status_code = api_exception_response(request, exc)
+        return api.create_response(request, response, status=status_code)
